d3bebc0a145582b9a294983fa523d4ad8b2e7b8a
Chris <example@example.com>
2017-01-02 15:58:29 -0800

    Test Food and Foodlist in SmartEatr.jsx

--- components/SmartEatr.jsx
+++ components/SmartEatr.jsx
@@ -1,7 +1,81 @@
 import React from 'react';
+import CalDisplay from 'CalDisplay';
+import DayList from 'DayList';
 
-const SmartEatr = () => (
-  <h1>Hello world</h1>
-);
+const SmartEatr = () => {
+  let days = [
+    {
+      id: 1,
+      foods: [
+        {
+          id: 1,
+          name: 'Chocolate Pie',
+          carbs: 24,
+          protein: 1,
+          fat: 23
+        },
+        {
+          id: 2,
+          name: 'Crisps',
+          carbs: 24,
+          protein: 1,
+          fat: 23
+        }
+      ]
+    },
+    {
+      id: 2,
+      foods: [
+        {
+          id: 5,
+          name: 'Tuna sandwich',
+          carbs: 1,
+          protein: 42,
+          fat: 0
+        },
+        {
+          id: 6,
+          name: 'Chicken sandwich',
+          carbs: 1,
+          protein: 4,
+          fat: 10
+        },
+        {
+          id: 432,
+          name: 'Candy bar',
+          carbs: 3,
+          protein: 1,
+          fat: 23
+        }
+      ]
+    },
+    {
+      id: 3,
+      foods: [
+        {
+          id: 5,
+          name: 'Light salad',
+          carbs: 12,
+          protein: 0,
+          fat: 0
+        },
+        {
+          id: 6,
+          name: 'Chicken sandwich',
+          carbs: 1,
+          protein: 4,
+          fat: 1
+        }
+      ]
+    },
+  ];
+
+  return (
+    <div>
+      <CalDisplay total={1350} goal={1900} />
+      <DayList days={days}/>
+    </div>
+  );
+};
 
 export default SmartEatr
