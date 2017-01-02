7826d69f63482495549a76516dfd071f89f61e9e
Chris <example@example.com>
2017-01-02 15:26:32 -0800

    Optimize bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// If the environment variable exists, keep it; otherwise set it to 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: './index.jsx',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
   output: {path: __dirname, filename: 'bundle.js'},
   resolve: {
     root: __dirname,
